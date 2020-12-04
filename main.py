da4c51eb707d8bc8c739d59e892b04128dee8f10
jpospinalo <jpospinalo@unal.edu.co>
2020-12-03 22:56:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,12 @@ async def auth_user(user_in: UserIn):
     return  {"Autenticado": True}
 
 
+
+@api.get("/")
+async def home():
+    return {"message": "¡Bienvenido a su cajero de confianza!"}
+
+
 @api.get("/user/balance/{username}")
 async def get_balance(username: str):
 
