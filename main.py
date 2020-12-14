43aae3cef27c224bbd624e7d35398b0ff53ef4d0
Monica Gomez <moagomezma@unal.edu.co>
2020-12-13 22:43:08 -0500

    act

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ api = FastAPI()
 
 origins = [
     "http://localhost",
-    "http://localhost:8081"
+    "http://localhost:8081",
+    "https://mcajero-ui.herokuapp.com/"
 ]
 
 api.add_middleware(
